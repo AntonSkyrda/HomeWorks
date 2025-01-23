9d22d4ebdd6ad7fff637df22db1d2f3a8858d4db
AntonSkyrda <mr.skyrda@gmail.com>
2025-01-23 23:59:11 +0200

    chenge queryset

--- courses/views.py
+++ courses/views.py
@@ -7,6 +7,15 @@ class CourseListView(ListView):
     model = Course
     context_object_name = "courses"
 
+    def get_queryset(self):
+        return Course.objects.prefetch_related(
+            "student",
+            "lessons",
+            "task_set",
+        ).select_related(
+            "teacher",
+        )
+
 
 class CourseDetailView(DetailView):
     model = Course
