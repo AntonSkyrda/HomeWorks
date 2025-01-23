13998434a5255eaee0194e5f8dd9445250ee65a1
AntonSkyrda <mr.skyrda@gmail.com>
2025-01-23 23:55:29 +0200

    Solution

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Course
+
+
+class CourseListView(ListView):
+    model = Course
+    context_object_name = "courses"
+
+
+class CourseDetailView(DetailView):
+    model = Course
--- lms/settings.py
+++ lms/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = "lms.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -95,6 +95,8 @@ USE_TZ = True
 
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = (BASE_DIR / "static",)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
